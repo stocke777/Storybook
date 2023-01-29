724f50a68e4807a511e4f163fea84b754360be1d
Jaivardhan Deshwal <deshwaljaivardhan@gmail.com>
2023-01-30 03:24:15 +0530

    framer motion added

--- storytime/src/App.js
+++ storytime/src/App.js
@@ -4,22 +4,39 @@ import { Routes, Route } from "react-router-dom";
 import NightRide from "./Pages/NightRide";
 import UpTheHill from "./Pages/UpTheHill";
 import WhiteLie from "./Pages/WhiteLie";
+import { useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import {motion} from 'framer-motion';
 
 function App() {
+	const location = useLocation();
 	return (
-		<Routes>
-			<Route
-				path='/'
-				element={
-					<div className='App'>
-						<HomePage></HomePage>
-					</div>
-				}
-			/>
-      <Route path="/NightRide/:page" element={<NightRide/>}/>
-      <Route path="/UpTheHill/:page" element={<UpTheHill/>}/>
-      <Route path="/WhiteLie/:page" element={<WhiteLie/>}/>
-		</Routes>
+		<AnimatePresence>
+			<Routes
+				location={location}
+				key={location.pathname}>
+				<Route
+					path='/'
+					element={
+						<motion.div className='App'initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+							<HomePage></HomePage>
+						</motion.div>
+					}
+				/>
+				<Route
+					path='/NightRide/:page'
+					element={<NightRide />}
+				/>
+				<Route
+					path='/UpTheHill/:page'
+					element={<UpTheHill />}
+				/>
+				<Route
+					path='/WhiteLie/:page'
+					element={<WhiteLie />}
+				/>
+			</Routes>
+		</AnimatePresence>
 	);
 }
 
